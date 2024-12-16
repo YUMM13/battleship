25c10df4c79dbbbce0b368716bb195da7d6f6342
YUMM13 <blakestambaugh1@gmail.com>
2024-12-16 14:12:33 -0700

    started creation of a tic tac toe server. The server can track game state and display it to console

--- cmd/battleship/main.go
+++ cmd/battleship/main.go
@@ -1,27 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"battleship/internal/TicTacToe"
 )
 
 func main() {
-	// create a new mux server
-	mux := http.NewServeMux()
-
-	// create the paths that will be handled by the server
-	mux.HandleFunc("GET /path/", homeHandler)
-
-	fmt.Println("Server is listening...")
-
-	// start the server and check for errors
-	err := http.ListenAndServe(":8090", mux)
-	if err != nil {
-		fmt.Println("Error starting server:", err)
-	}
-}
-
-// when someone sends a GET request to /path/, this function is called
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	tictactoe.CreateTicTacToeServer()
 }
